3f49d5d3cb56f6563f1e18ed7ef1558925da030c
Balkrushnagawai <krishnagawai2892@gmail.com>
2023-08-08 21:08:15 +0530

    [BalkrushnaGawai] Adding a new java program - Equilateral_triangle_with_space

---
+++ src/demo/Equilateral_download_with_space.java
@@ -0,0 +1,25 @@
+package demo;
+
+public class Equilateral_download_with_space {
+	public static void main(String[]args)
+	{
+		int star=9;
+		int space=1;
+		for(int i=1; i<=5; i++)
+		{
+			for (int a=1; a<=star; a++)
+			{
+				System.out.print(" *");
+			}
+			System.out.println();
+			System.out.println();
+			for(int a=1; a<=space; a++)
+			{
+				System.out.print(" ");
+			}
+			star=star-2;
+			space=space+2;
+		}
+	}
+
+}
